df5637503399d4a7c60d22c815dbfd8dcf2933c0
Aman Mittal <amandeepmittal@live.com>
2018-08-08 17:43:55 +0530

    🛠  ✔

---
+++ src/screens/FindMe/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class FindMe extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+	render() {
+		return (
+			<View>
+				<Text> Where am I? </Text>
+			</View>
+		);
+	}
+}
+
+export default FindMe;
